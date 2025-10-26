cdd69bd1032db0babb60855b96cfc515f383921c
HOPE-A1 <nekesahope5@gmail.com>
2025-10-26 15:04:22 +0300

    Add files via upload

---
+++ cylinder (1).py
@@ -0,0 +1,7 @@
+import math
+def cylinder_volume ():
+    radius=input("enter radius: ")
+    height=input("enter height: ")
+    volume=3.142*radius*radius*height
+    print("The voliume of cylinder is; ",volume)
+    cylinder_volume()
